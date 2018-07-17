ac79b7696087cfa7da1904cd9a88f3de7e8f53f6
flyingninja97 <40920084+flyingninja97@users.noreply.github.com>
2018-07-17 23:26:55 +0530

    Update phantom.py
    Web scraper in python for extracting high res poster from Imdb

--- phantom.py
+++ phantom.py
@@ -2,20 +2,26 @@ import os
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
-os.chdir('g:\pythonwork\ImdbpostersScapedd')
 
-driver = webdriver.PhantomJS(executable_path=r'C:\Users\Anany\Desktop\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\bin\phantomjs.exe')
+
+
+
+driver = webdriver.Chrome()
 k=1
 def scrap(url): 
+	
+	
 	global k 
 	driver.get(url)
 	soup=BeautifulSoup(driver.page_source,'lxml')
 	x=soup.find(class_='media_index_thumb_list')
 	y=x.find_all('a')
 	z=[]
+	
+	
 	for i in y:
 		z.append('https://www.imdb.com'+(i['href']))
-	dr = webdriver.PhantomJS(executable_path=r'C:\Users\Anany\Desktop\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\bin\phantomjs.exe')
+	dr = webdriver.Chrome()
 	for i in z:
 		dr.get(i)
 		soup=BeautifulSoup(dr.page_source,'lxml')
@@ -26,7 +32,6 @@ def scrap(url):
 			print(b[1])
 			k+=1
 			pic.write(requests.get(b[1]['src']).content)
-driver = webdriver.PhantomJS(executable_path=r'C:\Users\Anany\Desktop\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\bin\phantomjs.exe')
 for i in range (1,49):
 	url='https://www.imdb.com/gallery/rg1624939264?page=%d&ref_=rgmi_mi_sm'%(i)
 	scrap(url)
